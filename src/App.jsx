9035bea33bb0b83b945dec3c8bdbdeb2543a831e
vansh <vanshsharma88@gmail.com>
2024-03-12 02:43:56 +0530

    Add MemoryAddress component and add flagregister component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import "./components/MemoryAddress";
+import MemoryAddress from "./components/MemoryAddress";
+import FlagRegister from "./components/FlagRegister";
+
+function App() {
+  return (
+   <>
+   <MemoryAddress/>
+   <FlagRegister/>
+   </>
+  );
+}
+
+export default App;
